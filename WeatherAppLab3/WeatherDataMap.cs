f38ed015fc9797a8a1a6d4c276b51a4c875e3857
Theresa Dunca <146927903+tdunca@users.noreply.github.com>
2025-11-13 06:57:27 +0100

    Wrote WeatherDataMap

--- WeatherAppLab3/WeatherDataMap.cs
+++ WeatherAppLab3/WeatherDataMap.cs
@@ -1,8 +1,31 @@
-﻿using System;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+using System.Globalization;
+using WeatherAppLab3.Models;
 
-public class Class1
+namespace WeatherAppLab3
 {
-	public Class1()
-	{
-	}
+    public class WeatherDataMap : ClassMap<WeatherRecord>
+    {
+        public WeatherDataMap()
+        {
+            Map(m => m.Date).Name("Date").TypeConverterOption.Format("yyyy-MM-dd H:mm");
+            Map(m => m.Location).Name("Location");
+            Map(m => m.Temperature).Name("Temp").TypeConverter<NullableDoubleConverter>();
+            Map(m => m.Humidity).Name("Humidity").TypeConverter<NullableDoubleConverter>();
+        }
+    }
+    public class NullableDoubleConverter : DefaultTypeConverter
+    {
+        public override object? ConvertFromString(string? text, CsvHelper.IReaderRow row, MemberMapData memberMapData)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                return null;
+            }
+
+            return double.TryParse(text, NumberStyles.Any, CultureInfo.InvariantCulture, out var result) ? (double?)result : null;
+        }
+    }
 }
